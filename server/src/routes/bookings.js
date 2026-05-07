2589616b96370beff9cfaecc66c86e2f6d577337
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-05-07 17:00:57 +0500

    booking model and routes setup

---
+++ server/src/routes/bookings.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const {protect, admin} = require('../middlewares/auth');
+
+router.post('/send-otp', protect, sendBookingOTP);
+router.post('/', protect, bookEvent);
+router.put('/:id/confirm', protect, admin, confirmBooking);
+router.get('/my', protect, getMyBookings);
+router.delete('/:id', protect, cancelBooking);
+
+module.exports = router;
--- server/src/routes/events.js
+++ server/src/routes/events.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { protect, admin } = require("../middlewares/auth");
+const {
+  getEvents,
+  getEventById,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+} = require("../controllers/eventController");
 
 router.get("/", getEvents); //user see all events
 router.get("/:id", getEventById); // user see event details by id
