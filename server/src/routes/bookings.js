22bf3300eae8c0a6c7de30dae2b62bc5bba3f4ec
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-05-07 17:12:31 +0500

    booking controller setup

--- server/src/routes/bookings.js
+++ server/src/routes/bookings.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {protect, admin} = require('../middlewares/auth');
+const { protect, admin } = require("../middlewares/auth");
+const {
+  sendBookingOTP,
+  bookEvent,
+  confirmBooking,
+  getMyBookings,
+  cancelBooking,
+} = require("../controllers/bookingController");
 
-router.post('/send-otp', protect, sendBookingOTP);
-router.post('/', protect, bookEvent);
-router.put('/:id/confirm', protect, admin, confirmBooking);
-router.get('/my', protect, getMyBookings);
-router.delete('/:id', protect, cancelBooking);
+router.post("/send-otp", protect, sendBookingOTP);
+router.post("/", protect, bookEvent);
+router.put("/:id/confirm", protect, admin, confirmBooking);
+router.get("/my", protect, getMyBookings);
+router.delete("/:id", protect, cancelBooking);
 
 module.exports = router;
